fc6e33b0221877e892db536a69cdbe0d58ac6ec9
Filip Skoczeń <philipskoczen@gmail.com>
2022-07-24 10:02:36 +0200

    Make handmade number animation

--- src/components/HourPicker.vue
+++ src/components/HourPicker.vue
@@ -2,10 +2,26 @@
 const props = defineProps<{
   time: number
 }>();
+const old = ref(props.time);
+
+watch(() => props.time, (value) => {
+  const diff = Math.abs(value - old.value);
+
+  const interval = setInterval(() => {
+    if (old.value < value)
+      old.value++;
+
+    else if (old.value > value)
+      old.value--;
+
+    else
+      clearInterval(interval);
+  }, 500 / diff);
+});
 </script>
 
 <template>
-  <span>{{ props.time }}</span>
+  <span>{{ old }}</span>
   <span>hr</span>
-  <span v-if="props.time > 1">s</span>
+  <span v-if="old != 1">s</span>
 </template>
